29e0f9c36db1cf10aa743e76dffba7b3006e3b06
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-07-03 14:39:45 +0100

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
+import { cleanEnv, str, num } from 'envalid';
+
 export const env = cleanEnv(process.env, {
-  /** Database URL, must relate to a Prisma-supported database. See /prisma.schema for more info. */
   DATABASE_URL: str(),
-  /** Server port to run on. If you deploy this to a service, they'll override this. */
   PORT: num({ default: 3030 }),
-  /**
+});
