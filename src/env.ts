dee5d1249f0129fdb1fff782d34c075ae2872eda
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-07-03 16:41:47 +0100

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,13 @@
-import * as dotenv from 'dotenv';
+import { config as dotenvConfig } from "dotenv";
 
 // Load environment variables from a .env file
-dotenv.config();
+dotenvConfig();
 
 // Define and export an environment object with type annotations
 export const env = {
   isProduction: process.env.NODE_ENV === 'production',
-  port: parseInt(process.env.PORT || '3000', 10),
-  dbUri: process.env.DB_URI || '',
+  isDev: process.env.NODE_ENV === 'development',
+  PORT: parseInt(process.env.PORT || '3000', 10),
+  DB_URI: process.env.DB_URI || '',
   // Add other environment variables as needed
 };
