5b7a243708403a372ddf8575a4d4e0d871498979
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-07-03 16:56:50 +0100

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,13 @@
-import { config as dotenvConfig } from "dotenv";
+import { cleanEnv, num, str, url } from "envalid";
 
-// Load environment variables from a .env file
-dotenvConfig();
-
-// Define and export an environment object with type annotations
-export const env = {
-  isProduction: process.env.NODE_ENV === 'production',
-  isDev: process.env.NODE_ENV === 'development',
-  PORT: parseInt(process.env.PORT || '3000', 10),
-  DB_URI: process.env.DB_URI || '',
-  // Add other environment variables as needed
-};
+export const env = cleanEnv(process.env, {
+  /** Database URL, must relate to a Prisma-supported database. See /prisma.schema for more info. */
+  DATABASE_URL: str(),
+  /** Server port to run on. If you deploy this to a service, they'll override this. */
+  PORT: num({ default: 3030 }),
+  /**
+   * The maximum number of pages to fetch from a Letterboxd list. Ends up being around 980 movies for a user list, and 3500 for a "popular" list.
+   * Note that popular lists will only grab the first page.
+   * */
+  ADDON_MAX_PAGES_FETCHED: num({ default: 35 }),
+});
