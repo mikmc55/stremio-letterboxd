c2f3e1c4c380ec6c1c22e18acf857db24cedb29b
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-07-03 14:18:36 +0100

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,7 @@
-import { cleanEnv, num, str, url } from "envalid";
 
 export const env = cleanEnv(process.env, {
   /** Database URL, must relate to a Prisma-supported database. See /prisma.schema for more info. */
-  DATABASE_URL: sqlitecloud://ctdkbjjwsk.sqlite.cloud:8860?apikey=3gIzHb8VQ4LPboSQnpMPZNlTr1Q4HqKhhCON78gX9bI,
+  DATABASE_URL: str(),
   /** Server port to run on. If you deploy this to a service, they'll override this. */
   PORT: num({ default: 3030 }),
   /**
-   * The maximum number of pages to fetch from a Letterboxd list. Ends up being around 980 movies for a user list, and 3500 for a "popular" list.
-   * Note that popular lists will only grab the first page.
-   * */
-  ADDON_MAX_PAGES_FETCHED: num({ default: 35 }),
-});
