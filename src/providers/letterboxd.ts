57f3cf1b3f8d988c525b5894bd170108e6f577af
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-07-03 15:13:01 +0100

    Update letterboxd.ts

--- src/providers/letterboxd.ts
+++ src/providers/letterboxd.ts
@@ -1,268 +1,116 @@
-import { addonFetch } from "../lib/fetch.js";
-import { load as cheerio } from "cheerio";
 import { prisma } from "../prisma.js";
-import type { StremioMetaPreview } from "../consts.js";
+import { LRUCache } from "lru-cache";
+import {
+  config,
+  ONE_HOUR,
+  type StremioMeta,
+  type StremioMetaPreview,
+} from "../consts.js";
 import { logger } from "../logger.js";
-import { env } from "../env.js";
+const log = logger("lruCache");
 
-const logBase = logger("providers:letterboxd");
+const cache = new LRUCache<string, StremioMetaPreview[]>({
+  max: 100,
+  ttl: ONE_HOUR,
+  allowStale: true,
+});
 
-function generateKey() {
-  return (
-    Math.random().toString(36).substring(2, 15) +
-    Math.random().toString(36).substring(2, 15)
-  );
-}
+export const lruCache = {
+  save: async (id: string, providedMetas?: StremioMetaPreview[]) => {
+    console.info(
+      `[lrucache:save] Start saving ${id} with ${providedMetas?.length} movies provided.`,
+    );
+    const consoleTime = `lrucache:save ${id}`;
+    console.time(consoleTime);
 
-const base = env.isProd || env.isProduction ? "/" : "http://localhost:3030/";
-
-const getHtml = async (
-  letterboxdSlug: string,
-  overrideUrl?: string,
-): Promise<ReturnType<typeof cheerio> | undefined> => {
-  const log = logBase.extend("getHtml");
-
-  try {
-    const modifiedSlug = letterboxdSlug.replace(/ /g, "-");
-    const url = !overrideUrl
-      ? `https://letterboxd.com/film/${modifiedSlug}`
-      : overrideUrl.replace(/\{letterboxdSlug\}/g, modifiedSlug);
-    const res = await addonFetch(url, {
-      keepalive: false,
-    });
-    if (!res.ok) {
-      throw Error(`[${modifiedSlug}]: Couldn't get Letterboxd info: ${url}`);
+    // if metas provided, save early and quit
+    if (providedMetas?.length) {
+      cache.set(id, providedMetas);
+      log(`[lrucache:save] saved ${id} with ${providedMetas.length} metas`);
+      return true;
     }
 
-    const html = await res.text();
-    const $ = cheerio(html);
-    return $;
-  } catch (error) {
-    log(`[letterboxd] Couldn't get Letterboxd info: ${letterboxdSlug}`);
-  }
+    const movies = await (async (): Promise<string[] | undefined> => {
+      try {
+        // grab list
+        const user = await prisma.letterboxdUser.findUnique({
+          where: {
+            id,
+          },
+        });
+        try {
+          if (!user) {
+            throw `No user with the ID: ${id}`;
+          }
+        } catch (error) {
+          console.warn(`[lrucache:save] ${error}`);
+          return undefined;
+        }
 
-  return undefined;
-};
+        return JSON.parse(user.movie_ids) as string[];
+      } catch (error) {
+        console.error(`[lrucache:save] Couldn't get movie IDs`);
+      }
 
-/**
- * Updates the letterboxd posters.
- * @param letterboxdId Letterboxd slug
- * @returns
- */
-const updatePoster = async (letterboxdId: string, letterboxdPath: string) => {
-  const log = logBase.extend("updatePoster");
-  const stringifiedId = `${letterboxdId.replace(/ /g, "-")}`;
-  try {
-    const letterboxd = await prisma.letterboxdPoster.findUnique({
-      where: {
-        letterboxdId: stringifiedId,
-      },
-    });
+      return undefined;
+    })();
 
-    const shouldUpdate =
-      letterboxd &&
-      Date.now() - letterboxd.updatedAt.getTime() < 1000 * 60 * 60;
-
-    const create = !letterboxd;
-
-    if (!create && !shouldUpdate) {
-      // early exit, don't update
-      log(`Not updating poster for ${letterboxdId}, too recent.`);
+    if (!movies) {
+      console.warn(`[lrucache:save] Couldn't save ${id}`);
+      console.timeEnd(consoleTime);
       return undefined;
     }
 
-    log(
-      `${create ? "Creating a new poster" : "Updating poster"} for ${letterboxdId}`,
-    );
+    // get meta from movies
+    const metas: StremioMeta[] = [];
 
-    const $ = await getHtml(
-      stringifiedId,
-      `${base}poster/${encodeURIComponent(letterboxdPath)}/${stringifiedId}`,
-      // `https://letterboxd.com/ajax/poster/film/{letterboxdSlug}/std/1000x1500/?k=${generateKey()}`,
-    );
-
-    if (!$) {
-      log(
-        `Couldn't get poster from page for ${letterboxdId}: no html for cheerio to parse`,
-      );
-      return undefined;
-    }
-
-    const poster = $("img").first().attr("src");
-    if (!poster) {
-      throw `Couldn't get poster from page for ${letterboxdId}`;
-    }
-    const upserted = await prisma.letterboxdPoster.upsert({
-      where: {
-        letterboxdId: stringifiedId,
-      },
-      create: {
-        letterboxdId: stringifiedId,
-        // letterboxdPath,
-        url: poster,
-      },
-      update: {
-        url: poster,
-      },
+    // get all cinemeta, if we can
+    const cinemetas = await prisma.cinemeta.findMany({
+      where: { id: { in: movies } },
     });
 
-    if (!upserted) {
-      throw `Couldn't create poster entry for ${letterboxdId}`;
+    // sort the metas
+    for (const movie of movies) {
+      // find the cinemeta entry and push it to metas
+      const foundMovie = cinemetas.findIndex((m) => m.id === movie);
+      if (foundMovie === -1) continue;
+      const meta = JSON.parse(cinemetas[foundMovie].info) as StremioMeta;
+      metas.push(meta);
     }
 
-    return poster;
-  } catch (error) {
-    console.warn(error);
-  }
-
-  return undefined;
-};
-
-export async function find(
-  letterboxdSlug: string,
-  userId: string,
-): Promise<{ letterboxd: string; imdb: string; poster?: string } | undefined> {
-  const log = logBase.extend("find");
-  try {
-    let rv: { poster?: string } = {};
-    // template literal because prisma coerces strings to numbers
-    const db = await prisma.letterboxdIMDb.findUnique({
-      where: { letterboxd: `${letterboxdSlug}` },
-      include: { poster: true },
-    });
-    if (!db) {
-      throw `No record for ${letterboxdSlug}`;
-    }
-    if (db.poster) {
-      // await a poster, then continue
-      const poster = await updatePoster(letterboxdSlug, userId);
-      rv = { poster };
-    }
-
-    // Early return if we have a record already.
-    log(`Found L-IMDB: ${letterboxdSlug} -> ${db.imdb}`);
-    return {
-      letterboxd: db.letterboxd,
-      imdb: db.imdb,
-      ...rv,
-    };
-  } catch (error) {
-    if (typeof error === "string") {
-      log(error);
-    } else {
-      log(`Couldn't query db for some reason`);
-      log(error);
-    }
-  }
-
-  try {
-    const logLtoImdb = log.extend("lboxdtoimdb");
-    const $ = await getHtml(letterboxdSlug);
-
-    if (!$) return undefined;
-
-    const imdbUrl = $('a[data-track-action="IMDb"]').attr("href");
-    const rimdb = /tt[0-9]+/;
-
-    const match = imdbUrl?.match(rimdb);
-    if (!match) return undefined;
-
-    const id = match[0];
-
-    prisma.letterboxdIMDb
-      .create({
-        data: {
-          letterboxd: letterboxdSlug,
-          imdb: id,
-        },
-      })
-      .then(() =>
-        logLtoImdb(`Created letterboxd->imdb: ${[letterboxdSlug, id]}`),
-      )
-      .catch((err) => {
-        logLtoImdb(
-          `Prisma error creating letterboxd->imdb: ${letterboxdSlug} -> ${id}`,
-        );
-        logLtoImdb(err.message);
-      });
-
-    return { letterboxd: letterboxdSlug, imdb: id, poster: undefined };
-  } catch (error) {
-    log(error.message);
-  }
-
-  return undefined;
-}
-
-export const replacePosters = async (metas: StremioMetaPreview[]) => {
-  const log = logBase.extend("replacePosters");
-  try {
-    const letterboxdImdbIDs = await prisma.letterboxdIMDb.findMany({
-      where: {
-        imdb: {
-          in: metas.map((meta) => meta.id),
-        },
-      },
-      include: {
-        poster: true,
-      },
-    });
-
-    if (!letterboxdImdbIDs || letterboxdImdbIDs.length === 0) {
-      throw "No posters found or error.";
-    }
-
-    return metas.map((meta) => {
-      const found = letterboxdImdbIDs.findIndex((v) => v.imdb === meta.id);
-      if (found === -1) {
-        log("No letterboxd poster found to replace.");
-        return meta;
+    try {
+      // If more than 1000 entries, change the expiry time to 60 multiplied by the amount of movies to limit large fetches.
+      // const expires =
+      //   metas.length > TOO_MANY_MOVIES
+      //     ? Date.now() + 1000 * 60 * metas.length
+      //     : Date.now() + 1000 * 60 * 60;
+      console.info(`[lrucache:save]: saving ${id} with ${metas.length} metas`);
+      const cacheOpts: Parameters<typeof cache.set>[2] = {};
+      if (metas.length > config.filmThreshold) {
+        const amountOver = config.filmThreshold - metas.length;
+        const extraTTL = (ONE_HOUR / 2) * amountOver;
+        cacheOpts.ttl = extraTTL;
       }
+      cache.set(id, metas, cacheOpts);
+      return true;
+    } catch (error) {
+      console.error(`[lrucache:save] Couldn't save staticCache ${id}`);
+      console.error(error);
+      console.timeEnd(consoleTime);
+    }
+    console.timeEnd(consoleTime);
+    return false;
+  },
 
-      if (letterboxdImdbIDs[found].poster.length === 0) {
-        log(`No letterboxd posters in database for ${meta.id}`);
-        return meta;
-      }
-
-      return {
-        ...meta,
-        poster: letterboxdImdbIDs[found].poster[0].url,
-      };
-    });
-  } catch (error) {
-    log("Couldn't update with Letterboxd posters");
-    log(error);
-  }
-
-  return metas;
+  get: (id: string) => {
+    console.info(`[lrucache:get] Trying to get ${id} static cache`);
+    try {
+      const metas = cache.get(id);
+      return metas;
+    } catch (error) {
+      console.error(`[lrucache:get] Couldn't parse meta JSON`);
+      console.error(error);
+    }
+    return undefined;
+  },
 };
-
-export async function find_old(letterboxdSlug: string) {
-  // https://letterboxd.com/ajax/poster/film/wait-until-dark/std/125x187/?k=851e7b94
-  try {
-    const url = `https://letterboxd.com/ajax/poster/film/${letterboxdSlug.replace(
-      / /gi,
-      "-",
-    )}/std/125x187/?k=${Date.now()}`;
-    const res = await addonFetch(url);
-    if (!res.ok) {
-      throw Error(`[${letterboxdSlug}]: Couldn't get Letterboxd info: ${url}`);
-    }
-    const rawHtml = await res.text();
-    const $ = cheerio(rawHtml);
-
-    const slug = $("div").data("filmSlug") as string;
-    const year = $("div").data("filmReleaseYear") as string;
-    const name = $("div").data("filmName") as string;
-    let poster = $("img").prop("srcset") as string;
-    if (poster) {
-      poster = poster.replace(/-250-/g, "-400-").replace(/-375-/g, "-600-");
-    }
-
-    return { slug, name, year, poster };
-  } catch (error) {
-    console.error(error);
-  }
-
-  return undefined;
-}
