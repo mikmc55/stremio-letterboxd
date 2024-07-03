abe892e8e5a9c6e860e3b793a37de71501265607
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-07-03 14:55:27 +0100

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,9 @@
-import { cleanEnv, str, num } from 'envalid';
+import dotenv from "dotenv";
 
-export const env = cleanEnv(process.env, {
-  DATABASE_URL: str(),
-  PORT: num({ default: 3030 }),
-});
+dotenv.config();
+
+export const env = {
+  DATABASE_URL: process.env.DATABASE_URL,
+  PORT: Number(process.env.PORT),
+  ADDON_MAX_PAGES_FETCHED: Number(process.env.ADDON_MAX_PAGES_FETCHED), // Added
+} as const;
